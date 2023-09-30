7076abfb74439c574ec5fde3bf9b66b2c3fdb23f
milad <miladarabali1@gmail.com>
2023-09-30 20:02:42 +0330

    fix bug

--- src/app/core/guard/is-admin.guard.ts
+++ src/app/core/guard/is-admin.guard.ts
@@ -1,29 +1,37 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import {CookieServiceLogin} from "../../shared/services/cookie.service";
-import {UserLoginService} from "../../components/user-authentication/sign-in-user/shared/services/user-login.service";
-
-
-
-
-@Injectable({
-  providedIn: 'root'
-})
-export class IsAdminGuard implements CanActivate {
-
-  constructor(private auth: CookieServiceLogin
-              , private router: Router,
-              private userLogin:UserLoginService) {
-  }
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ):
-    | Observable<boolean | UrlTree>
-    | Promise<boolean | UrlTree>
-    | boolean
-    | UrlTree {
-   return true
-  }
-}
+// import { Injectable } from '@angular/core';
+// import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';;
+// import {CookieServiceLogin} from "../../shared/services/cookie.service";
+// import {CookieService} from "ngx-cookie-service";
+//
+//
+//
+//
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class IsAdminGuard implements CanActivate {
+//
+//   // @ts-ignore
+//   constructor(private router: Router,
+//               private cookie: CookieService,
+//               private auth: CookieServiceLogin,
+//               private userDataService: UserDataService) {
+//   }
+//
+//   canActivate(route: ActivatedRouteSnapshot,
+//               state: RouterStateSnapshot): Promise<boolean | UrlTree> | boolean | UrlTree {
+//     if (this.auth.isLogin() || (this.cookie.check('username'))) {
+//       this.userDataService.adminStatus.subscribe(result => {
+//         if (result) {
+//           return true;
+//         } else {
+//           this.router.navigate(['/home']);
+//           return false;
+//         }
+//       });
+//     } else {
+//       this.router.navigate(['/sign-in']);
+//     }
+//     return false;
+//   }
+// }
